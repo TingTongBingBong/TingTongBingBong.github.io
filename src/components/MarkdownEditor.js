0b253c4d85dda5fa069f77c963cfa955313b0ede
Malcolm <robert.malcolm.mcniven@gmail.com>
2024-08-14 12:54:10 -0400

    fixing usestate error

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Split from 'react-split';
 import ReactMarkdown from 'react-markdown';
 import './MarkdownEditor.css';
--- src/components/NoteSubPage.js
+++ src/components/NoteSubPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MarkdownEditor from './MarkdownEditor';
 
 const NoteSubPage = ({ title }) => {
