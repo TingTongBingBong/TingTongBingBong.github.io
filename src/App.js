1faa8717c8f2c38a290953cb0d0d523cb6bf7e2e
Malcolm <robert.malcolm.mcniven@gmail.com>
2024-08-14 14:00:35 -0400

    simplifying

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Navbar from './components/Navbar'; // Ensure Navbar component is properly
 import HomePage from './components/HomePage'; // Ensure HomePage component is properly defined
 import NotesPage from './components/NotesPage'; // Ensure NotesPage component is properly defined
 import SubPage1 from './pages/SubPage1'; // Ensure SubPage1 component is properly defined
-import SubPage2 from './pages/SubPage2'; // Ensure SubPage2 component is properly defined
-import SubPage3 from './pages/SubPage3'; // Ensure SubPage3 component is properly defined
 import './App.css'; // Global styles
 
 function App() {
