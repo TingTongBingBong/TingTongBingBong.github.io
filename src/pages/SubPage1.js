de71a989308bf6b89fafc86e8850fc43045f612e
Malcolm <robert.malcolm.mcniven@gmail.com>
2024-08-14 12:36:32 -0400

    Fixing subpages

--- src/pages/SubPage1.js
+++ src/pages/SubPage1.js
@@ -2,7 +2,7 @@ import React from 'react';
 import NoteSubPage from '../components/NoteSubPage';
 
 const SubPage1 = () => <NoteSubPage title="SubPage 1" />;
-const SubPage2 = () => <NoteSubPage title="SubPage 2" />;
-const SubPage3 = () => <NoteSubPage title="SubPage 3" />;
 
-export { SubPage1, SubPage2, SubPage3 };
+
+export default SubPage1;
+
--- src/pages/SubPage2.js
+++ src/pages/SubPage2.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import NoteSubPage from '../components/NoteSubPage';
 
-const SubPage1 = () => <NoteSubPage title="SubPage 1" />;
-const SubPage2 = () => <NoteSubPage title="SubPage 2" />;
-const SubPage3 = () => <NoteSubPage title="SubPage 3" />;
 
-export { SubPage1, SubPage2, SubPage3 };
+const SubPage2 = () => <NoteSubPage title="SubPage 2" />;
+
+
+export default SubPage2;
+
