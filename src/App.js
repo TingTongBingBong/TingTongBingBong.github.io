00d613047073433f8e8db27d7f246c7dbc52c463
Malcolm <robert.malcolm.mcniven@gmail.com>
2024-08-14 14:51:39 -0400

    working finally

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar'; // Ensure Navbar component is properly defined
 import HomePage from './components/HomePage'; // Ensure HomePage component is properly defined
-//import NotesPage from './components/NotesPage'; // Ensure NotesPage component is properly defined
-//import SubPage1 from './pages/SubPage1'; // Ensure SubPage1 component is properly defined
+import NotesPage from './components/NotesPage'; // Ensure NotesPage component is properly defined
+import SubPage1 from './pages/SubPage1'; // Ensure SubPage1 component is properly defined
 
-//<Route path="/notes" element={<NotesPage />} />
-//<Route path="/notes/subpage1" element={<SubPage1 />} />
 
 import './App.css'; // Global styles
 
@@ -17,7 +15,8 @@ function App() {
       <div className="app-container">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          
+          <Route path="/notes" element={<NotesPage />} />
+          <Route path="/notes/subpage1" element={<SubPage1 />} />
         </Routes>
       </div>
     </Router>
